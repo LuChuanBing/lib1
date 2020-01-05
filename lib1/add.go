3a4eeb8ffbf76ce31d7faa83314783dbd8176cd7
luchuanbing <luchuanbing123@163.com>
2020-01-05 17:10:05 +0800

    lib1

---
+++ lib1/add.go
@@ -0,0 +1,7 @@
+package lib1
+
+import "github.com/LuChuanBing/lib0/math"
+
+func Add() {
+	math.Add(1, 2)
+}
---
+++ lib1/add_test.go
@@ -0,0 +1,11 @@
+package lib1
+
+import (
+	"testing"
+
+	"github.com/LuChuanBing/lib0/math"
+)
+
+func Test11(t *testing.T) {
+	math.Add(1, 2)
+}
